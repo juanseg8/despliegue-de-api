b4a0a6bf9dfbfb6926e97060e5ed119ea838547d
JuanSe Guilisasti <jguilisasti503@gmail.com>
2023-10-04 20:10:59 -0300

    despliegue de api

---
+++ src/main/java/com/desafio/desafio3/Models/DTOs/UserReadDTO.java
@@ -0,0 +1,14 @@
+package com.desafio.desafio3.Models.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserReadDTO {
+    Integer id;
+    String nombre;
+    Integer celular;
+}
---
+++ src/main/java/com/desafio/desafio3/Models/Repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.desafio.desafio3.Models.Repositories;
+
+import com.desafio.desafio3.Models.Entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Boolean existsByNombre(String nombre);
+
+    Boolean existsByCelular(Integer celular);
+}
